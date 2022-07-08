10e1a1a022d3de2655eefa48559c158789f79493
Rakesh Rewale <104151669+rakeshrewale@users.noreply.github.com>
2022-07-08 12:58:28 +0530

    Update AppTest.java

--- src/test/java/com/Cab_Invoice_Generator/AppTest.java
+++ src/test/java/com/Cab_Invoice_Generator/AppTest.java
@@ -1,20 +1,45 @@
 package com.Cab_Invoice_Generator;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class CabInvoiceGeneratorTest {
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double totalFare = cabInvoiceGenerator.calculateFare(5.0, 4);
+		Assert.assertEquals(54, totalFare, totalFare);
+	}
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnMinFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double totalFare = cabInvoiceGenerator.calculateFare(0.1, 2);
+		Assert.assertEquals(5, totalFare, totalFare);
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldCalculateAggregateTotal() {
+
+		Rides[] rides = { new Rides(5, 4), new Rides(0.1, 2) };
+
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double totalFare = cabInvoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(54, totalFare, totalFare);
+	}
+
+	@Test
+	public void ReturnTotalRidesTotalFareandAverageFareperRide() {
+		Rides[] rides = { new Rides(5, 4), new Rides(0.1, 2) };
+
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double totalFare = cabInvoiceGenerator.calculateFare(rides);
+		int noOfRides = cabInvoiceGenerator.numberOfRides(rides);
+		double averageFare = cabInvoiceGenerator.calculateAverageFarePerRide(rides);
+
+		Assert.assertEquals(54, totalFare, totalFare);
+		Assert.assertEquals(2, noOfRides);
+		Assert.assertEquals(27, averageFare, averageFare);
+	}
 }
